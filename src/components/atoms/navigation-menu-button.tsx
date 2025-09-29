26971b9ea8f3d8e34f36bbf11d9a272935b2caaa
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2025-09-29 14:50:33 +0700

    create initial navbar

--- src/components/atoms/navigation-menu-button.tsx
+++ src/components/atoms/navigation-menu-button.tsx
@@ -1,37 +1,120 @@
-import { MenuItems } from "@/configs";
+"use client";
+
+import { cn } from "@/lib/utils";
+import { ChevronDown } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Suspense } from "react";
+import { match, P } from "ts-pattern";
+import { MergeExclusive } from "type-fest";
+
+export type MenuItems = {
+  label: string;
+} & MergeExclusive<
+  { href: string },
+  {
+    submenus:
+      | Exclude<MenuItems, "submenus">[]
+      | (() => Promise<Exclude<MenuItems, "submenus">[]>);
+  }
+>;
 
 type Props = MenuItems;
 
 export function NavigationMenuButton(props: Props) {
-  if (props.submenus) {
-    return (
-      <div key={props.label} className="relative group">
-        <button className="text-white hover:text-amber-400 transition-colors">
-          {props.label}
-        </button>
-        <div className="absolute opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-black border border-gray-200 mt-2 py-2 w-48 z-10">
-          {props.submenus.map((submenu) => (
-            <Link
-              key={submenu.label}
-              href={submenu.href ?? "#"}
-              className="block px-4 py-2 text-white hover:bg-gray-700"
-            >
-              {submenu.label}
-            </Link>
-          ))}
-        </div>
-      </div>
-    );
-  }
+  const pathname = usePathname();
+
+  const isPathMatchCurrentButton =
+    props.href && pathname.includes(props.href ?? "");
+
+  const classNames = cn(
+    "text-sm lg:text-base text-center",
+    "h-full text-white hover:bg-secondary hover:text-white transition-colors min-w-10  px-2",
+    "flex flex-col justify-center hover:cursor-pointer items-center",
+
+    isPathMatchCurrentButton && "text-secondary",
+
+    "md:min-w-20"
+  );
 
   return (
-    <Link
-      key={props.label}
-      href={props.href ?? "#"}
-      className="text-white hover:text-amber-400 transition-colors"
-    >
-      {props.label}
-    </Link>
+    match(props)
+      /**
+       * jika ada href, maka render Link
+       */
+      .with({ href: P.string }, (props) => {
+        return (
+          <Link
+            key={props.label}
+            href={props.href}
+            className={cn(classNames, "")}
+          >
+            {props.label}
+          </Link>
+        );
+      })
+
+      /**
+       * jika ada submenus, maka render dropdown
+       */
+      .with({ submenus: P.nonNullable }, (props) => {
+        return (
+          <div key={props.label} className="relative group h-full">
+            <button className={cn(classNames, "flex flex-row items-center")}>
+              {props.label}{" "}
+              <ChevronDown size={16} fill="currentColor" className="pt-1" />
+            </button>
+            <div
+              className={cn(
+                "absolute hidden group-hover:block transition-transform duration-300",
+                "bg-[#353535]/90 border left-0 py-2 min-w-[326px] z-10",
+                "border-[#353535] backdrop-blur-lg rounded-b-lg"
+              )}
+            >
+              {match(props.submenus)
+                /**
+                 * jika submenus berupa array, maka render langsung
+                 */
+                .with(P.array(), (submenus) =>
+                  submenus.map((submenu) => (
+                    <Link
+                      key={submenu.label}
+                      href={submenu.href ?? "#"}
+                      className="block px-4 py-2 text-white hover:font-bold"
+                    >
+                      {submenu.label}
+                    </Link>
+                  ))
+                )
+
+                /**
+                 * jika submenus berupa function yang mengembalikan Promise,
+                 * maka render dengan Suspense
+                 */
+                .otherwise((submenusPromise) => {
+                  const submenus = submenusPromise().catch(() => []);
+
+                  return (
+                    <Suspense fallback={<div>Loading...</div>}>
+                      {submenus.then((data) =>
+                        (data ?? []).map((submenu) => (
+                          <Link
+                            key={submenu.label}
+                            href={submenu.href ?? "#"}
+                            className="block px-4 py-2 text-white hover:font-bold text-sm lg:text-base"
+                          >
+                            {submenu.label}
+                          </Link>
+                        ))
+                      )}
+                    </Suspense>
+                  );
+                })}
+            </div>
+          </div>
+        );
+      })
+
+      .exhaustive()
   );
 }
--- src/components/organism/visitor-navbar.tsx
+++ src/components/organism/visitor-navbar.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { NavigationMenus } from "@/components/molecules";
+import { LanguageSwitch } from "@/components/molecules/language-switch";
 import { useAppLocale } from "@/hooks";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { PropsWithChildren } from "react";
@@ -12,11 +14,22 @@ export default function VisitorNavbar({}: Props) {
   const {} = useAppLocale();
 
   return (
-    <nav className="fixed min-h-20 top-0 left-0 w-full bg-black border-b-amber-400 border-2 border-gray-200 flex items-center justify-between px-6 py-4 z-50 opacity-90 backdrop-blur-xl">
-      <Link href="/" className="font-semibold text-lg">
-        <Image src="/svgs/logo.svg" alt="Logo" width={100} height={40} />
+    <nav className="fixed min-h-24 w-full bg-primary border-b-secondary border-2 border-gray-200 flex items-center justify-between z-50 opacity-90 backdrop-blur-xl">
+      <Link href="/" className="font-semibold text-lg px-6 py-2">
+        <Image
+          className="w-34 h-auto"
+          src="/svgs/logo.svg"
+          alt="Logo"
+          width={100}
+          height={80}
+        />
       </Link>
-      <NavigationMenus />
+      <div className={cn("hidden h-20", "md:block")}>
+        <NavigationMenus />
+      </div>
+      <div className={cn("px-6 py-4 hidden", "md:block")}>
+        <LanguageSwitch />
+      </div>
     </nav>
   );
 }
