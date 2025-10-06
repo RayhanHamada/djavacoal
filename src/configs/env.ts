8f321bd7a11cab12674ef603e220934a5e0fe971
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2025-10-06 23:58:56 +0700

    refactor: update Cloudflare environment types and restructure auth handling

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -2,18 +2,36 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod/v4";
 
 export const env = createEnv({
-  emptyStringAsUndefined: true,
-  experimental__runtimeEnv: {
-    NODE_ENV: process.env.NODE_ENV,
-    NEXT_PUBLIC_ASSET_URL: process.env.NEXT_PUBLIC_ASSET_URL,
-  },
   shared: {
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
+    NODE_ENV: z.enum(["development", "production"]),
+
+    BETTER_AUTH_SECRET: z.string(),
+    BETTER_AUTH_URL: z.url(),
+    BETTER_AUTH_BASE_PATH: z.string().default("/api/auth"),
+
+    RESEND_API_KEY: z.string(),
   },
-  server: {},
+  server: {
+    CLOUDFLARE_ACCOUNT_ID: z.string(),
+    CLOUDFLARE_DATABASE_ID: z.uuid(),
+    CLOUDFLARE_D1_TOKEN: z.string(),
+  },
   client: {
     NEXT_PUBLIC_ASSET_URL: z.url().optional(),
+    NEXT_PUBLIC_BASE_URL: z.url().default("http://localhost:3000"),
   },
+
+  experimental__runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+
+    NEXT_PUBLIC_ASSET_URL: process.env.NEXT_PUBLIC_ASSET_URL,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    BETTER_AUTH_BASE_PATH: process.env.BETTER_AUTH_BASE_PATH,
+
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+  },
+  emptyStringAsUndefined: true,
 });
---
+++ src/lib/auth/server.ts
@@ -0,0 +1,88 @@
+import "server-only";
+
+import {
+  TABLE_NAMES,
+  ACCOUNT_COLUMNS,
+  COMMON_COLUMNS,
+  SESSION_COLUMNS,
+  USER_COLUMNS,
+  VERIFICATION_COLUMNS,
+} from "@/adapters/d1/constants";
+import { getDB } from "@/adapters/d1/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { emailOTP } from "better-auth/plugins";
+
+const plugins = [
+  emailOTP({
+    async sendVerificationOTP({}) {},
+  }),
+];
+
+export const getAuth = (db: D1Database) =>
+  betterAuth({
+    database: drizzleAdapter(getDB(db), {
+      provider: "sqlite",
+    }),
+    user: {
+      modelName: TABLE_NAMES.USERS,
+      fields: {
+        name: USER_COLUMNS.NAME,
+        email: USER_COLUMNS.EMAIL,
+        emailVerified: USER_COLUMNS.EMAIL_VERIFIED,
+        image: USER_COLUMNS.IMAGE,
+
+        createdAt: COMMON_COLUMNS.CREATED_AT,
+        updatedAt: COMMON_COLUMNS.UPDATED_AT,
+      },
+    },
+    session: {
+      modelName: TABLE_NAMES.SESSIONS,
+      fields: {
+        userId: SESSION_COLUMNS.USER_ID,
+        token: SESSION_COLUMNS.TOKEN,
+        expiresAt: SESSION_COLUMNS.EXPIRES_AT,
+        ipAddress: SESSION_COLUMNS.IP_ADDRESS,
+        userAgent: SESSION_COLUMNS.USER_AGENT,
+
+        createdAt: COMMON_COLUMNS.CREATED_AT,
+        updatedAt: COMMON_COLUMNS.UPDATED_AT,
+      },
+    },
+    account: {
+      modelName: TABLE_NAMES.ACCOUNTS,
+      fields: {
+        userId: ACCOUNT_COLUMNS.USER_ID,
+        accountId: ACCOUNT_COLUMNS.ACCOUNT_ID,
+        providerId: ACCOUNT_COLUMNS.PROVIDER_ID,
+        accessToken: ACCOUNT_COLUMNS.ACCESS_TOKEN,
+        refreshToken: ACCOUNT_COLUMNS.REFRESH_TOKEN,
+        accessTokenExpiresAt: ACCOUNT_COLUMNS.ACCESS_TOKEN_EXPIRES_AT,
+        refreshTokenExpiresAt: ACCOUNT_COLUMNS.REFRESH_TOKEN_EXPIRES_AT,
+        scope: ACCOUNT_COLUMNS.SCOPE,
+        idToken: ACCOUNT_COLUMNS.ID_TOKEN,
+        password: ACCOUNT_COLUMNS.PASSWORD,
+
+        createdAt: COMMON_COLUMNS.CREATED_AT,
+        updatedAt: COMMON_COLUMNS.UPDATED_AT,
+      },
+    },
+    verification: {
+      modelName: TABLE_NAMES.VERIFICATIONS,
+      fields: {
+        identifier: VERIFICATION_COLUMNS.IDENTIFIER,
+        value: VERIFICATION_COLUMNS.VALUE,
+        expiresAt: VERIFICATION_COLUMNS.EXPIRES_AT,
+
+        createdAt: COMMON_COLUMNS.CREATED_AT,
+        updatedAt: COMMON_COLUMNS.UPDATED_AT,
+      },
+    },
+    emailAndPassword: {
+      enabled: true,
+    },
+
+    plugins,
+  });
+
+export type Auth = ReturnType<typeof getAuth>;
