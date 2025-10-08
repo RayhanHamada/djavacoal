8b3f57fc1807b9f6f419302f8a4b11d2a785036a
Ardinatahs <ardinata.harisaputra29@gmail.com>
2025-10-08 19:30:58 +0700

    add contactUs page

---
+++ src/features/contactUs/components/atoms/SectionTitle.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const SectionTitle = ({ text }: { text: string }) => (
+  <h2 className="text-white text-2xl md:text-3xl font-semibold text-center">
+    {text}
+  </h2>
+);
---
+++ src/features/contactUs/components/molecules/index.ts
@@ -0,0 +1,4 @@
+export * from "./ContactInfoItem";
+export * from "./ContactSocial";
+export * from "./ContactMap";
+export * from "./ContactForm";
