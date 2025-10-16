ba7c57084ddf129fee4a0ad9a24826b87fbc1044
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2025-10-16 16:50:31 +0700

    feat: add reset and forgot password feature

--- src/features/admin-auth/lib/form-schema.ts
+++ src/features/admin-auth/lib/form-schema.ts
@@ -60,12 +60,39 @@ export const SetPasswordFormSchema = z
     path: ["confirmPassword"],
   });
 
+export const ForgotPasswordFormSchema = z.object({
+  email: z.email("Invalid email address").nonempty("Email is required"),
+});
+
+export const ResetPasswordFormSchema = z
+  .object({
+    password: z
+      .string()
+      .nonempty("Password is required")
+      .min(8, "Password must be at least 8 characters")
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
+        "Password must contain at least one uppercase letter, one lowercase letter, and one number"
+      ),
+    confirmPassword: z.string().nonempty("Please confirm your password"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
 export const validateOnboardingForm = zod4Resolver(OnboardingFormSchema);
 export const validateLoginForm = zod4Resolver(LoginFormSchema);
 export const validateInviteAdminForm = zod4Resolver(InviteAdminFormSchema);
 export const validateSetPasswordForm = zod4Resolver(SetPasswordFormSchema);
+export const validateForgotPasswordForm = zod4Resolver(
+  ForgotPasswordFormSchema
+);
+export const validateResetPasswordForm = zod4Resolver(ResetPasswordFormSchema);
 
 export type OnboardingFormValues = z.infer<typeof OnboardingFormSchema>;
 export type LoginFormValues = z.infer<typeof LoginFormSchema>;
 export type InviteAdminFormValues = z.infer<typeof InviteAdminFormSchema>;
 export type SetPasswordFormValues = z.infer<typeof SetPasswordFormSchema>;
+export type ForgotPasswordFormValues = z.infer<typeof ForgotPasswordFormSchema>;
+export type ResetPasswordFormValues = z.infer<typeof ResetPasswordFormSchema>;
