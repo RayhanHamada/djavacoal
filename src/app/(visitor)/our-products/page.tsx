c0c2a7d7097dce889db03b857f1a14b0ceb55461
kludsh <yozanirwan@gmail.com>
2025-10-18 00:06:43 +0700

    no message

---
+++ src/app/(visitor)/our-products/page.tsx
@@ -0,0 +1,10 @@
+import { ProductPage } from "@/features/our-products/components";
+import { Fragment } from "react";
+
+export default async function OurProducts() {
+  return (
+    <Fragment>
+      <ProductPage />
+    </Fragment>
+  );
+}
--- src/app/(visitor)/page.tsx
+++ src/app/(visitor)/page.tsx
@@ -15,6 +15,17 @@ import { Fragment } from "react";
 export default async function Home() {
   return (
     <Fragment>
+      <div className="mx-auto mb-20 mt-10 max-w-2xl bg-primary px-4 text-center sm:mb-32 sm:mt-32 sm:px-6 lg:px-8">
+        <h1 className="font-bold text-h1 sm:text-4xl" dir="rtl">
+          شحذ طاقتك مع الحل الأمثل للطاقة النظيفة
+        </h1>
+        <h1 className="font-bold text-h1-size text-secondary sm:text-4xl">
+          Djavacoal Indonesia:
+        </h1>
+        <h1 className="font-bold text-h1 sm:text-4xl" dir="rtl">
+          شحذ طاقتك مع
+        </h1>
+      </div>
       <BannerSection />
       <LogoSlideSection />
       <AboutUsSection />
