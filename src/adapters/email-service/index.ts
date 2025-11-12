ffe91e3a8c071bbd62ca9633309904876c122599
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2025-11-12 14:24:21 +0700

    feat: use react email

--- src/adapters/email-service/index.ts
+++ src/adapters/email-service/index.ts
@@ -1,5 +1,18 @@
+import { createTransport as createNodemailerTransport } from "nodemailer";
 import { Resend } from "resend";
 
 export function getResend(apiKey: string) {
     return new Resend(apiKey);
+}
+
+export function getNodemailerTransporter() {
+    const transporter = createNodemailerTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS,
+        },
+    });
+
+    return transporter;
 }
