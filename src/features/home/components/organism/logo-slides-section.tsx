ba2b19d8d2eb5c96a35ccc6f8f8c5493c2131a89
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2025-11-23 19:37:09 +0700

    fix: update class properties for better responsiveness in LogoSlideSection component

--- src/features/home/components/organism/logo-slides-section.tsx
+++ src/features/home/components/organism/logo-slides-section.tsx
@@ -24,7 +24,7 @@ export function LogoSlideSection() {
     const repeated = Array(8).fill(base).flat();
 
     const Card = ({ src, w, h }: { src: string; w: number; h: number }) => (
-        <div className="flex h-[150px] w-[320px] flex-shrink-0 items-center justify-center border border-[#2A2A2A] bg-[radial-gradient(circle_at_center,#000_0%,#171717_50%,_#ffffff10_100%)] shadow-[0_0_20px_#00000030] transition-all duration-300 hover:border-[#EFA12D] hover:shadow-[#EFA12D]/20">
+        <div className="flex h-[150px] w-[320px] shrink-0 items-center justify-center border border-[#2A2A2A] bg-[radial-gradient(circle_at_center,#000_0%,#171717_50%,#ffffff10_100%)] shadow-[0_0_20px_#00000030] transition-all duration-300 hover:border-[#EFA12D] hover:shadow-[#EFA12D]/20">
             <Image
                 src={src}
                 alt={t("logoAlt")}
@@ -36,8 +36,8 @@ export function LogoSlideSection() {
     );
 
     return (
-        <section className="relative w-full overflow-hidden bg-gradient-to-b from-[#0D0D0D] to-[#1A1A1A]">
-            <div className="pointer-events-none absolute inset-0 bg-[radial-gradient(circle_at_center,_#000_0%,_#0D0D0D_80%,_#1A1A1A_100%)] opacity-30" />
+        <section className="relative w-full overflow-hidden bg-linear-to-b from-[#0D0D0D] to-[#1A1A1A]">
+            <div className="pointer-events-none absolute inset-0 bg-[radial-gradient(circle_at_center,#000_0%,#0D0D0D_80%,#1A1A1A_100%)] opacity-30" />
 
             {/* ✅ Tambahkan wrapper ini agar di mobile bisa di-scroll */}
             <div className="scrollbar-hide relative h-[150px] w-full touch-pan-x overflow-hidden overflow-x-auto md:overflow-hidden">
