9a10e52cac81cd0a07c523e5906abc9cfe51cb94
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2025-11-26 07:26:02 +0700

    refactor: convert BlogContent to forwardRef for improved ref handling

--- src/features/blog/components/atoms/blog-content.tsx
+++ src/features/blog/components/atoms/blog-content.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from "react";
 import "./blog-content.css";
 
 import { cn } from "@/lib/utils";
@@ -7,21 +8,24 @@ interface BlogContentProps {
     className?: string;
 }
 
-export function BlogContent({ content, className }: BlogContentProps) {
-    return (
-        <div
-            className={cn(
-                "prose prose-invert max-w-none py-10",
-                "font-open-sans text-base leading-[1.36em] font-normal text-[#C6C6C6]",
-                "text-wrap [&_p]:overflow-clip [&_p]:text-wrap [&>h1]:mb-4 [&>h2]:mb-4 [&>h3]:mb-4 [&>p]:mb-6",
-                "[&>h2]:text-2xl [&>h3]:text-xl [&>h4]:text-lg",
-                "[&>h2]:font-semibold [&>h3]:font-semibold [&>h4]:font-semibold",
-                "[&_li]:mb-2 [&_ol]:list-decimal [&_ol]:pl-6 [&_ul]:list-disc [&_ul]:pl-6",
-                "[&_blockquote]:mb-6 [&_blockquote]:border-l-4 [&_blockquote]:border-gray-600 [&_blockquote]:pl-4 [&_blockquote]:text-gray-400 [&_blockquote]:italic",
-                "[&_a]:text-blue-400 [&_a]:underline [&_a:hover]:text-blue-500",
-                className
-            )}
-            dangerouslySetInnerHTML={{ __html: content }}
-        />
-    );
-}
+export const BlogContent = forwardRef<HTMLDivElement, BlogContentProps>(
+    function _({ content, className }, ref) {
+        return (
+            <div
+                ref={ref}
+                className={cn(
+                    "prose prose-invert max-w-none py-10",
+                    "font-open-sans text-base leading-[1.36em] font-normal text-[#C6C6C6]",
+                    "text-wrap [&_p]:overflow-clip [&_p]:text-wrap [&>h1]:mb-4 [&>h2]:mb-4 [&>h3]:mb-4 [&>p]:mb-6",
+                    "[&>h2]:text-2xl [&>h3]:text-xl [&>h4]:text-lg",
+                    "[&>h2]:font-semibold [&>h3]:font-semibold [&>h4]:font-semibold",
+                    "[&_li]:mb-2 [&_ol]:list-decimal [&_ol]:pl-6 [&_ul]:list-disc [&_ul]:pl-6",
+                    "[&_blockquote]:mb-6 [&_blockquote]:border-l-4 [&_blockquote]:border-gray-600 [&_blockquote]:pl-4 [&_blockquote]:text-gray-400 [&_blockquote]:italic",
+                    "[&_a]:text-blue-400 [&_a]:underline [&_a:hover]:text-blue-500",
+                    className
+                )}
+                dangerouslySetInnerHTML={{ __html: content }}
+            />
+        );
+    }
+);
