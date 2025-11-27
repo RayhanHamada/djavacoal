93c8bfa3a6b2d22cd3bc4006fa052eea343cecf7
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2025-11-27 19:31:41 +0700

    chore: fix map

--- src/features/contact-us/components/molecules/contact-map.tsx
+++ src/features/contact-us/components/molecules/contact-map.tsx
@@ -1,12 +1,10 @@
 export function ContactMap() {
     return (
         <iframe
-            src="https://www.google.com/maps?q=Taiba+Cococha+Indonesia,+Tajurhalang,+Bogor,+West+Java&z=13&output=embed"
-            className="h-full w-full rounded-lg grayscale filter transition duration-500 ease-in-out hover:scale-[1.05] hover:grayscale-0"
-            style={{ border: 0 }}
-            allowFullScreen
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.3120666838836!2d110.72521280000001!3d-7.5409051!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a15004c3164cd%3A0xac187c8c3e62cd72!2sDjavacoal%20Indonesia%20-%20Charcoal%20Factory!5e0!3m2!1sen!2sid!4v1764246566129!5m2!1sen!2sid"
+            width="600"
+            height="450"
             loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
+        />
     );
 }
